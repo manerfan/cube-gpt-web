7775f4ac687f18381ebcfa67ca0b3a2e4dcee730
Maner·Fan <manerfan@163.com>
2023-10-09 14:07:01 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2023 Maner·Fan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import Image from 'next/image'
 
 export default function Home() {
@@ -5,23 +21,24 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+        <code className="font-mono font-bold">CUBE CHAT</code>
+        &nbsp;|&nbsp;
+        <code className="font-mono">Speek <span className="font-bold">FREELY</span> with Me!</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://cubewisebit.github.io/"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/logo.png"
+              alt="CubeBit Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={60}
+              height={60}
               priority
             />
           </a>
@@ -31,13 +48,45 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
+          src="/logo.png"
+          alt="CubeBit Logo"
           width={180}
-          height={37}
+          height={180}
           priority
         />
+        <span className='space-x-4'>
+          <code className="font-mono font-bold text-2xl">. CUBE CHAT</code>
+          <span>|</span>
+          <span className="font-mono">Speek <span className="font-bold">FREELY</span> with Me!</span>
+        </span>
+        
       </div>
+
+      <form className="container my-8 lg:w-1/2 md:w-3/4 sm:w-auto">
+        <label htmlFor="chat-input" className="sr-only">Enter your prompt and Chat with me!</label>
+        <div className="relative">
+          <input
+            id="chat-input"
+            className="block w-full resize-none rounded-xl border-none bg-slate-100 p-4 pr-16 text-sm text-slate-900 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-slate-800 dark:text-slate-200 dark:placeholder-slate-400 dark:focus:ring-blue-500 sm:text-base"
+            placeholder="Enter your prompt and Chat with me!"
+            required
+          />
+          <button
+            type="submit"
+            className="absolute bottom-2 right-2.5 rounded-lg bg-blue-100 p-2 text-sm font-medium text-slate-200 hover:bg-blue-200 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:text-base"
+          >
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/logo.png"
+              alt="CubeBit Logo"
+              width={24}
+              height={24}
+              priority
+            />
+            <span className="sr-only">Send message</span>
+          </button>
+        </div>
+      </form>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
